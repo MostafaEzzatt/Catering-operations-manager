de216a24123eef1492e291075b3cb72cbbaacce5
MostafaEzzatt <psmostafa@gmail.com>
2026-02-07 16:37:56 +0200

    finish delete company

--- actions/customer.ts
+++ actions/customer.ts
@@ -3,6 +3,8 @@
 
 import { db } from "@/drizzle";
 import { cutomersTable } from "@/drizzle/db/schema";
+import { eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 
 export async function addCustomer(prevState: any, value: CompanyFormValues) {
   try {
@@ -10,6 +12,19 @@ export async function addCustomer(prevState: any, value: CompanyFormValues) {
 
     await db.insert(cutomersTable).values(customer).returning();
 
+    revalidatePath("/add-companys");
+    return true;
+  } catch (error) {
+    console.error("Insertion failed:", error);
+    return false;
+  }
+}
+
+export async function deleteCustomer(prevState: any, value: number) {
+  try {
+    await db.delete(cutomersTable).where(eq(cutomersTable.id, value));
+
+    revalidatePath("/add-companys");
     return true;
   } catch (error) {
     console.error("Insertion failed:", error);
--- components/company-table.tsx
+++ components/company-table.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { deleteCustomer } from "@/actions/customer";
 import { cutomersTable } from "@/drizzle/db/schema";
 import {
   ColumnDef,
@@ -8,6 +9,10 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 import { InferSelectModel } from "drizzle-orm";
+import { Trash } from "lucide-react";
+import { useActionState, useEffect, useTransition } from "react";
+import { toast } from "sonner";
+import { Button } from "./ui/button";
 import {
   Table,
   TableBody,
@@ -22,6 +27,20 @@ const CompanyTable = ({
 }: {
   customers: InferSelectModel<typeof cutomersTable>[];
 }) => {
+  const [deleteState, deleteSubmit, deleteIsPending] = useActionState(
+    deleteCustomer,
+    null,
+  );
+  const [transitionPending, startTransition] = useTransition();
+
+  const isPending = transitionPending || deleteIsPending;
+
+  function handleDelete(id: number) {
+    startTransition(() => {
+      deleteSubmit(id);
+    });
+  }
+
   const columns: ColumnDef<InferSelectModel<typeof cutomersTable>>[] = [
     {
       id: "index", // Required for display columns
@@ -36,13 +55,38 @@ const CompanyTable = ({
       accessorKey: "code",
       header: "كود الشركة",
     },
+    {
+      accessorKey: "Delete",
+      header: "Delete",
+      cell: (info) => {
+        return (
+          <Button
+            onClick={() => handleDelete(info.row.original.id)}
+            disabled={isPending}
+          >
+            <Trash />
+          </Button>
+        );
+      },
+    },
   ];
 
+  // eslint-disable-next-line react-hooks/incompatible-library
   const table = useReactTable({
     columns,
     data: customers,
     getCoreRowModel: getCoreRowModel(),
   });
+
+  useEffect(() => {
+    if (isPending) {
+      toast.info("نعمل على مسح الشركة");
+    } else if (!isPending && deleteState == true) {
+      toast.success("تم مسح الشركة.");
+    } else if (!isPending && deleteState === false) {
+      toast.error("حدث خطاء ما عند مسح الشركة.");
+    }
+  }, [isPending, deleteState]);
 
   return (
     <div className="overflow-hidden rounded-md border">
@@ -82,7 +126,7 @@ const CompanyTable = ({
           ) : (
             <TableRow>
               <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
+                لم يتم اضافة شركات.
               </TableCell>
             </TableRow>
           )}
