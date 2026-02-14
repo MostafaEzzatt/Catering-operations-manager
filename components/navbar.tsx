f35d0a636c7e0dcdf2f20ef1bbbbc7ac2103e6d3
MostafaEzzatt <psmostafa@gmail.com>
2026-02-14 14:53:33 +0200

    fix navbar problem

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,6 @@
 "use client";
 
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
-import { cn } from "@/lib/utils";
 import Link from "next/link";
-import * as React from "react";
 import { ModeToggle } from "./ui/theme-toggle";
 
 const navList = [
@@ -23,24 +14,21 @@ const Navbar = () => {
     <nav className="px-6 py-3 border-b border-dashed mb-8 print:hidden">
       <div className="flex justify-between items-center container mx-auto">
         {/* Navigation Section */}
-        <NavigationMenu dir="rtl">
-          <NavigationMenuList>
-            {navList.map((link) => (
-              <NavigationMenuItem key={link.href}>
-                <Link href={link.href} passHref legacyBehavior>
-                  <NavigationMenuLink
-                    className={cn(
-                      navigationMenuTriggerStyle(),
-                      "text-base font-medium", // Optional: adjusting font for Arabic readability
-                    )}
-                  >
-                    {link.text}
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            ))}
-          </NavigationMenuList>
-        </NavigationMenu>
+
+        <ul className="flex gap-4">
+          {navList.map((i) => (
+            <li key={i.href}>
+              <Link
+                className={
+                  "group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=open]:hover:bg-accent data-[state=open]:text-accent-foreground data-[state=open]:focus:bg-accent data-[state=open]:bg-accent/50 focus-visible:ring-ring/50 outline-none transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1"
+                }
+                href={i.href}
+              >
+                {i.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
 
         {/* Action Section */}
         <div className="flex items-center gap-4">
